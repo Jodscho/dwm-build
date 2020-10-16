251fd350c77999db5b9d958e76d41ede397bccc7
Jodscho <Jonathan2008@gmx.de>
2020-10-16 19:22:34 +0200

    undo transparency

--- config.def.h
+++ config.def.h
@@ -2,12 +2,14 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 0;       /* vertical padding of bar */
+static const int sidepad            = 0;       /* horizontal padding of bar */
+//static const int vertpad            = 10;       /* vertical padding of bar */
+//static const int sidepad            = 15;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Droid Sans Mono:size=12" };
 static const char dmenufont[]       = "Droid Sans Mono:size=12";
 static const char col_gray1[]       = "#222222";
@@ -17,17 +19,10 @@ static const char col_gray4[]       = "#eeeeee";
 static const char my_col[]          = "#1c1c1c";
 static const char my_green[]        = "#b5bd68";
 static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
 	[SchemeSel]  = { my_green, my_col,  col_gray2  },
-};
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
