c120d8293a415a5a85efdaa188ef8ce16c5d373f
Jodscho <Jonathan2008@gmx.de>
2020-11-01 11:16:35 +0100

    patch: center floating windows

--- config.def.h
+++ config.def.h
@@ -34,10 +34,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "XTerm",  NULL,       "helper_notes.sh",       0,       1,           -1 },
+	/* class      instance    title       tags mask     iscentered     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,       1,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,      0,           -1 },
+	{ "XTerm",  NULL,       "helper_notes.sh",       0,       1,        1,           -1 },
 };
 
 /* layout(s) */
