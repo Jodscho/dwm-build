e7f6db4727e0bbda68d973b69f3232ebb6596e31
Jodscho <Jonathan2008@gmx.de>
2020-11-01 11:07:11 +0100

    start helper notes in floating mode

--- config.def.h
+++ config.def.h
@@ -37,6 +37,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "XTerm",  NULL,       "helper_notes.sh",       0,       1,           -1 },
 };
 
 /* layout(s) */
